61a48ba19a40970f9c9948abeea117402071f041
Salvatore Tosti <saltosti@gmail.com>
2017-06-30 19:17:44 -0700

    cleaned up unused dependency

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.cli "0.3.5"]
-                 [clj-http "3.6.1"]]
+                 [org.clojure/tools.cli "0.3.5"]]
   :plugins [[lein-kibit "0.1.2"]
             [lein-bin "0.3.5"]]
   :main ^:skip-aot harbor.core
